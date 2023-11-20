08bd23832bbbffbebb8ecc77e78651baac04063d
Rafiere <rafapgbr@gmail.com>
2023-11-20 13:47:24 -0300

    feat: add initial project files

---
+++ src/main/java/dev/piccodev/daarquiteturaaodeploy/DaarquiteturaaodeployApplication.java
@@ -0,0 +1,13 @@
+package dev.piccodev.daarquiteturaaodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaarquiteturaaodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaarquiteturaaodeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/piccodev/daarquiteturaaodeploy/DaarquiteturaaodeployApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.piccodev.daarquiteturaaodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaarquiteturaaodeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
